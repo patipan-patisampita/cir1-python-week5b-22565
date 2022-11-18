96299016ac945a7ef085618a12d85fe88bd9976f
patipan-patisampita <patipan.patisampita@gmail.com>
2022-11-18 11:22:01 +0700

    update

---
+++ week506.py
@@ -0,0 +1,9 @@
+# num = 4
+num = float(input("Enter number: "))
+if num >= 0:
+    if num == 0:
+        print(num, "is Zero")
+    else:
+        print(num, "is Positive")
+else:
+    print(num, "is Negative number")
